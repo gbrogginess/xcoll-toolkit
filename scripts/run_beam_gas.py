5355173960da04baf1b3890658b555054b9d90f8
gbrogginess <giacomo.broggi@cern.ch>
2025-03-07 19:10:53 +0100

    Update `run_beam_gas.py`.

--- scripts/run_beam_gas.py
+++ scripts/run_beam_gas.py
@@ -16,7 +16,6 @@ from copy import deepcopy
 from warnings import warn
 from collections import namedtuple
 from pathlib import Path
-from multiprocessing import Pool
 from contextlib import contextmanager
 
 import xobjects as xo
@@ -218,6 +217,7 @@ def insert_missing_bounding_apertures(line):
 
 def deactivate_bg_elems(line):
     # Deactivate beam-gas elements replacing them with dummy markers
+    line.discard_tracker()
     newLine = xt.Line(elements=[], element_names=[])
 
     for ee, nn in zip(line.elements, line.element_names):
@@ -239,6 +239,9 @@ def _configure_tracker_radiation(line, radiation_model, beamstrahlung_model=None
     _beamstrahlung_model = None if beamstrahlung_model == 'off' else beamstrahlung_model
     _bhabha_model = None if bhabha_model == 'off' else bhabha_model
 
+    if line.tracker is None:
+        line.build_tracker()
+
     if radiation_model == 'mean':
         if for_optics:
             # Ignore beamstrahlung and bhabha for optics
@@ -247,9 +250,6 @@ def _configure_tracker_radiation(line, radiation_model, beamstrahlung_model=None
             line.configure_radiation(model=radiation_model, 
                                      model_beamstrahlung=_beamstrahlung_model,
                                      model_bhabha=_bhabha_model)
-         # The matrix stability tolerance needs to be relaxed for radiation and tapering
-         # TODO: check if this is still needed
-        line.matrix_stability_tol = 0.5
 
     elif radiation_model == 'quantum':
         if for_optics:
@@ -260,7 +260,6 @@ def _configure_tracker_radiation(line, radiation_model, beamstrahlung_model=None
             line.configure_radiation(model='quantum',
                                      model_beamstrahlung=_beamstrahlung_model,
                                      model_bhabha=_bhabha_model)
-        line.matrix_stability_tol = 0.5
 
     elif radiation_model == 'off':
         pass
@@ -313,22 +312,19 @@ def load_config(config_file):
 
 
 def _compensate_energy_loss(line, delta0=0.):
-    _configure_tracker_radiation(line, 'mean')
+    _configure_tracker_radiation(line, 'mean', for_optics=True)
     line.compensate_radiation_energy_loss(delta0=delta0)
+    line.discard_tracker()
 
 
-def _prepare_matched_beam(config_dict, line, ref_particle, element, emitt_x, emitt_y, num_particles, capacity):
+def _prepare_matched_beam(config_dict, line, twiss, ref_particle, element, emitt_x, emitt_y, num_particles, capacity):
     print(f'Preparing a matched Gaussian beam at {element}')
     sigma_z = config_dict['beam']['sigma_z']
-    radiation_mode =  config_dict['run'].get('radiation', 'off')
-
-    _configure_tracker_radiation(line, radiation_mode, for_optics=True)
 
     x_norm, px_norm = xp.generate_2D_gaussian(num_particles)
     y_norm, py_norm = xp.generate_2D_gaussian(num_particles)
     
     # The longitudinal closed orbit needs to be manually supplied for now
-    twiss = line.twiss(**XTRACK_TWISS_KWARGS)
     element_index = line.element_names.index(element)
     zeta_co = twiss.zeta[element_index] 
     delta_co = twiss.delta[element_index] 
@@ -358,8 +354,9 @@ def _prepare_matched_beam(config_dict, line, ref_particle, element, emitt_x, emi
     return part
 
 
-def generate_xpart_particles(config_dict, line, ref_particle, start_elem, capacity):
+def generate_xpart_particles(config_dict, line, twiss, ref_particle, start_elem, capacity):
     num_particles = config_dict['run']['nparticles']
+    radiation_mode = config_dict['run'].get('radiation', 'off')
 
     emittance = config_dict['beam']['emittance']
     if isinstance(emittance, dict): # Normalised emittances
@@ -367,7 +364,9 @@ def generate_xpart_particles(config_dict, line, ref_particle, start_elem, capaci
     else:
         ex = ey = emittance
 
-    particles = _prepare_matched_beam(config_dict, line, ref_particle, start_elem,  
+    _configure_tracker_radiation(line, radiation_mode, for_optics=True)
+
+    particles = _prepare_matched_beam(config_dict, line, twiss, ref_particle, start_elem,  
                                       ex, ey, num_particles, capacity)
 
     # TODO: Add offsets here
@@ -379,32 +378,12 @@ def generate_xpart_particles(config_dict, line, ref_particle, start_elem, capaci
     return particles
 
 
-def prepare_particles(config_dict, line, ref_particle, start_elem):
+def prepare_particles(config_dict, line, twiss, ref_particle, start_elem):
     capacity = config_dict['run']['max_particles']
 
-    particles = generate_xpart_particles(config_dict, line, ref_particle, start_elem, capacity)
+    particles = generate_xpart_particles(config_dict, line, twiss, ref_particle, start_elem, capacity)
 
     return particles
-
-
-def build_collimation_tracker(line):
-    # Chose a context
-    context = xo.ContextCpu()  # Only support CPU for Geant4 coupling TODO: maybe not needed anymore?
-    # Transfer lattice on context and compile tracking code
-    global_aper_limit = 1e3  # Make this large to ensure particles lost on aperture markers
-
-    # compile the track kernel once and set it as the default kernel. TODO: a bit clunky, find a more elegant approach
-
-    line.build_tracker(_context=context)
-
-    tracker_opts=dict(track_kernel=line.tracker.track_kernel,
-                      _buffer=line.tracker._buffer,
-                      _context=line.tracker._context,
-                      io_buffer=line.tracker.io_buffer)
-    
-    line.discard_tracker() 
-    line.build_tracker(**tracker_opts)
-    line.config.global_xy_limit=global_aper_limit
 
 
 def get_particle_info(particle_name):
@@ -412,6 +391,20 @@ def get_particle_info(particle_name):
     charge, A, Z, _ = xp.pdg.get_properties_from_pdg_id(pdg_id)
     mass = xp.pdg.get_mass_from_pdg_id(pdg_id)
     return ParticleInfo(particle_name, pdg_id, mass, A, Z, charge)
+
+
+def load_colldb(colldb, emit_dict):
+    if colldb.endswith('.json'):
+        colldb = xc.CollimatorDatabase.from_json(colldb,
+                                                    nemitt_x=emit_dict['x'],
+                                                    nemitt_y=emit_dict['y'])
+    elif colldb.endswith('.dat'):
+        colldb = xc.CollimatorDatabase.from_SixTrack(colldb,
+                                                nemitt_x=emit_dict['x'],
+                                                nemitt_y=emit_dict['y'])
+    else:
+        raise ValueError('Unknown collimator database format: {}. Must be .json or .dat'.format(colldb))
+    return colldb
 
 
 def load_and_process_line(config_dict):
@@ -434,19 +427,15 @@ def load_and_process_line(config_dict):
     # Load the line and compute optics
     line = xt.Line.from_json(inp['xtrack_line'])
     line.particle_ref = ref_part
-
-    # # Check that all aperture markers are set
-    # apercheck = line.check_aperture()
-    # from IPython import embed; embed()
     
     rf_cavities = line.get_elements_of_type(xt.elements.Cavity)[0]
 
     if run.get('turn_rf_off', False):
         print('Turning RF cavities off (set voltage to 0)')
-        for cv in rf_cavities:
-            cv.voltage = 0
+        for cav in rf_cavities:
+            cav.voltage = 0
 
-    if not any((cv.voltage > 0 for cv in rf_cavities)) or not any((cv.frequency > 0 for cv in rf_cavities)):
+    if not any((cav.voltage > 0 for cav in rf_cavities)) or not any((cav.frequency > 0 for cav in rf_cavities)):
         assert not comp_eloss, 'Cannot compensate SR energy loss with cavities off'
         print('RF cavities have no voltage or frequency, Twiss will be 4D')
         XTRACK_TWISS_KWARGS['method'] = '4d'
@@ -462,26 +451,18 @@ def load_and_process_line(config_dict):
         # If energy loss compensation is required, taper the lattice
         print('Compensating synchrotron energy loss (tapering mangets)')
         comp_eloss_delta0 = run.get('sr_compensation_delta', 0.0)
-        _compensate_energy_loss(optics_line, comp_eloss_delta0)
-        line = optics_line.copy()
+        _compensate_energy_loss(line, comp_eloss_delta0)
+    else:
+        # Build and discard the tracker. Needed to have all the element slices with ._parent attribute
+        # TODO: possibly remove this when xsuite issue #551 (https://github.com/xsuite/xsuite/issues/551) is resolved
+        # NOTE: if comp_eloss is True, the tracker is built and discarded in _compensate_energy_loss
+        line.build_tracker()
+        line.discard_tracker()    
 
-    # Build and discard the tracker. Needed to have all the element slices with ._parent attribute
-    # TODO: remove this when xsuite issue #551 (https://github.com/xsuite/xsuite/issues/551) is resolved
-    line.build_tracker()
-    line.discard_tracker()  
-
-    if inp['collimator_file'].endswith('.dat'):
-        colldb = xc.CollimatorDatabase.from_SixTrack(inp['collimator_file'],
-                                                    nemitt_x=emittance['x'],
-                                                    nemitt_y=emittance['y'])
-    elif inp['collimator_file'].endswith('.json'):
-        colldb = xc.CollimatorDatabase.from_json(inp['collimator_file'],
-                                                 nemitt_x=emittance['x'],
-                                                 nemitt_y=emittance['y'])
+    colldb = load_colldb(inp['collimator_file'], emittance)
     
     colldb.install_geant4_collimators(line=line, verbose=True)
-    
-    line.build_tracker()
+
     _configure_tracker_radiation(line, radiation_mode, for_optics=True)
     twiss = line.twiss(**XTRACK_TWISS_KWARGS)
     print('Assigning optics to collimators...')
@@ -650,20 +631,12 @@ def prepare_lossmap(particles, line, s0, binwidth, weights):
     return lm_dict
 
 
-def run(config_file_path, config_dict, line, bgman, particles, ref_part, start_element, s0):
+def run(config_file_path, config_dict, line, bgman, particles, start_element, s0):
     radiation_mode = config_dict['run']['radiation']
     beamstrahlung_mode = config_dict['run']['beamstrahlung']
     bhabha_mode = config_dict['run']['bhabha']
 
     nturns = config_dict['run']['turns']
-    
-    _configure_tracker_radiation(line, radiation_mode, for_optics=True)
-
-    emittance = config_dict['beam']['emittance']
-    if isinstance(emittance, dict):
-        emit = (emittance['x'], emittance['y'])
-    else:
-        emit = (emittance, emittance)
     
     _configure_tracker_radiation(line, radiation_mode, beamstrahlung_mode, bhabha_mode, for_optics=False)
     if 'quantum' in (radiation_mode, beamstrahlung_mode, bhabha_mode):
@@ -689,9 +662,7 @@ def run(config_file_path, config_dict, line, bgman, particles, ref_part, start_e
         print(f'Start turn {turn}, Survivng particles: {particles._num_active_particles}')
 
         if turn == 1:
-            line.discard_tracker()
             deactivate_bg_elems(line)
-            build_collimation_tracker(line)
             _configure_tracker_radiation(line, radiation_mode, beamstrahlung_mode, bhabha_mode, for_optics=False)
 
         line.track(particles, ele_start=start_element, ele_stop=start_element, num_turns=1)
@@ -708,9 +679,8 @@ def run(config_file_path, config_dict, line, bgman, particles, ref_part, start_e
     print(f'Tracking {nturns} turns done in: {time.time()-t0} s')
 
     output_file = Path(config_dict['run'].get('outputfile', 'part.hdf'))
-    output_dir = output_file.parent
-    # This is new
     output_file = config_file_path.parent / output_file
+    output_dir = output_file.parent
     output_dir = config_file_path.parent / output_dir
     if not os.path.exists(output_dir):
         # If the output directory does not exist, create it
@@ -739,9 +709,9 @@ def run(config_file_path, config_dict, line, bgman, particles, ref_part, start_e
                          weights=None, # energy weights?
                          weight_function=None)
     particles = LossMap.part
-    # Save xcoll loss map
-    fpath = output_dir / 'lossmap.json'
-    LossMap.to_json(fpath)
+    # # Save xcoll loss map
+    # fpath = output_dir / 'lossmap.json'
+    # LossMap.to_json(fpath)
     del LossMap
 
     # Make collimasim loss map
@@ -761,14 +731,13 @@ def run(config_file_path, config_dict, line, bgman, particles, ref_part, start_e
 def execute(config_file_path, config_dict):
     config_dict = CONF_SCHEMA.validate(config_dict)
 
-    line, ref_part, bgman, start_elem, s0 = load_and_process_line(config_dict)
-    build_collimation_tracker(line)
+    line, twiss, ref_part, bgman, start_elem, s0 = load_and_process_line(config_dict)
 
-    particles = prepare_particles(config_dict, line, ref_part, start_elem)
+    particles = prepare_particles(config_dict, line, twiss, ref_part, start_elem)
 
     bgman.initialise_particles(particles)
 
-    run(config_file_path, config_dict, line, bgman, particles, ref_part, start_elem, s0)
+    run(config_file_path, config_dict, line, twiss, bgman, particles, start_elem, s0)
 
 
 @contextmanager
